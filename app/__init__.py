7ac38fefe80caa85c19d6423b4c058df8c84b5a0
DracoTato <160530153+DracoTato@users.noreply.github.com>
2025-03-05 22:02:08 +0200

    Create config classes for each environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from typing import Any
-from .config import Config
+from os import getenv
+from .config import config_dict
 from .logging import setup_logging
 from .routes import register_blueprints
 from .db import db
-from .db.commands import register_commands  
+from .db.commands import register_commands
 
 
 def create_app(config: dict[str, Any] = {}):
@@ -14,11 +15,14 @@ def create_app(config: dict[str, Any] = {}):
         template_folder="web/templates",
     )
 
-    if config:
-        app.config.from_mapping(config)  # Highest priority
-    else:
-        conf = Config()  # Init the object first
-        app.config.from_object(conf)  # Load from file
+    config_env = config.get("ENV", None)
+    env = config_env if config_env else getenv("FLASK_ENV", "production")
+
+    conf = config_dict[env]
+
+    app.config.from_object(conf)
+    app.config.update(config)  # Highest priority
+
     setup_logging(app)
 
     db.init_app(app)  # Register db w/ flask instance
