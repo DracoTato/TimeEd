22eae0839543b3c632ae0fedeec1e8603e813de8
DracoTato <mohamedibrahim.codes@gmail.com>
2025-03-05 16:44:54 +0200

    Refactor the models into a modular structure; Add new models

---
+++ app/db/schema/__init__.py
@@ -0,0 +1,13 @@
+from .models import (
+    User,
+    Group,
+    Session,
+    SessionOccurrence,
+    CancelledSession,
+    Enroll,
+    Question,
+    Answer,
+    AbsenceRequest,
+)
+
+from .enums import User_Type, Account_Status, Gender
