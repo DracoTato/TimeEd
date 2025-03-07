3127d8fb854617bc9845e78b0b92bd2fd57a2a92
DracoTato <160530153+DracoTato@users.noreply.github.com>
2025-03-07 13:53:02 +0200

    Format: format files

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from .auth import auth_bp
 # list of blueprints to be registered in app/__init__.py
 BLUEPRINTS = [auth_bp]
 
+
 def register_blueprints(app):
     """Register blueprints with a flask instance"""
     for bp in BLUEPRINTS:
