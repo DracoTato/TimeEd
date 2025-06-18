f0c999fcb359cd8a1e19ed76e8e8fc8f68f668c6
DracoTato <160530153+DracoTato@users.noreply.github.com>
2025-06-18 22:29:16 +0300

    Refactor(routes): remove unused js files

--- app/web/assets/js/send_email.js
+++
@@ -1,8 +0,0 @@
-function sendEmail() {
-    const textArea = document.querySelector("#email-subject")
-    const email = document.querySelector("#email").getAttribute("href")
-    let subject = "About TimeED"
-    let body = textArea.value
-    window.open(`${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`)
-
-}
